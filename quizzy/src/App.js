aeb7a53f35cb4e89bd1f5956b398a318029a1439
Kshitij Gupta <guptakshitij2308@gmail.com>
2023-10-13 23:01:05 +0530

    Created fake api and fetched data from it handling diffenret cases for status using useReducer hook.

--- quizzy/src/App.js
+++ quizzy/src/App.js
@@ -1,8 +1,48 @@
+import { useEffect, useReducer } from "react";
 import "./App.css";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "loading":
+      return { ...state, status: "loading" };
+    case "ready":
+      return { ...state, status: "ready" };
+    case "active":
+      return { ...state, status: "active" };
+    case "finished":
+      return { ...state, status: "finished" };
+    default:
+      throw new Error("Action type not supported");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    fetchQuestions();
+  }, []);
+
   return (
     <div className="app">
       <Header />
