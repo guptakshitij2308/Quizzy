913fd157383de7f41ad3c4e8a3aff772c58cb145
Kshitij Gupta <guptakshitij2308@gmail.com>
2023-10-15 00:16:13 +0530

    Implemented the functionality of adding the answer and evaluation it correctly using new state answer, point and action (newAnswer).

--- quizzy/src/components/App.js
+++ quizzy/src/components/App.js
@@ -11,6 +11,8 @@ const initialState = {
   questions: [],
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -23,6 +25,18 @@ function reducer(state, action) {
       return { ...state, status: "loading" };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions[state.index];
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
     case "finished":
       return { ...state, status: "finished" };
     default:
@@ -31,7 +45,7 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -61,7 +75,13 @@ function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
--- quizzy/src/components/Options.js
+++ quizzy/src/components/Options.js
@@ -1,8 +1,18 @@
-function Options({ options }) {
+function Options({ options, dispatch, answer, question }) {
+  const hasBeenAnswered = answer !== null;
+
   return (
     <div className="options">
-      {options.map((option) => (
-        <button className="btn btn-option" key={option}>
+      {options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasBeenAnswered &&
+            (index === question.correctOption ? "correct" : "wrong")
+          }`}
+          key={option}
+          disabled={hasBeenAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
